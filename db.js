fee00b9f1e11bfc1428e783544a602bd6ceae2f1
Shivansh Soni <officialsonishivansh@gmail.com>
2023-09-25 20:11:04 +0530

    new

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
-const connection = mongoose.connect(`mongodb+srv://shivansh:soni@cluster0.kewn6jo.mongodb.net/fullstackmock1?retryWrites=true&w=majority`)
+const connection = mongoose.connect(`mongodb+srv://shivansh:soni@cluster0.2sdbctt.mongodb.net/fullstack5?retryWrites=true&w=majority`)
 
 module.exports = {
     connection
